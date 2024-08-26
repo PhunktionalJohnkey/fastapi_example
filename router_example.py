4c8511482a7094dcc9d706cc9433c497fc78b091
Olaleye Alex-Oni <olaleyealex-oni@olaleyes-mbp.home>
2024-08-26 16:24:54 +0100

    additional APIRouter class added...

--- router_example.py
+++ router_example.py
@@ -3,3 +3,20 @@ router = APIRouter()
 @router.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@router.get("/authors/{author_id}")
+async def read_author(author_id: int):
+    return {
+        "author_id": author_id,
+        "name": "Ernest Hemingway"
+    }
+
+@router.get("/books")
+async def read_books(year: int = None):
+    if year:
+        return {
+            "year": year,
+            "books": ["Book 1", "Book 2"]
+        }
+    return {"books": ["All Books"]}
