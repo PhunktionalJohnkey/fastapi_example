36d04414ae95411bd2c63fe6dde137477c9e19a0
PhunktionalJohnkey <leyealexoni@gmail.com>
2024-08-27 16:29:39 +0000

    adds pydantic model and db schema

--- carsharing.py
+++ carsharing.py
@@ -1,20 +1,11 @@
-from datetime import datetime
-from fastapi import FastAPI
-from fastapi import HTTPException
+from fastapi import FastAPI, HTTPException
+
+from schemas import load_db
+
 
 app = FastAPI()
 
-db = [
-    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
-    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
-    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
-    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
-    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
-    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
-    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
-    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
-    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
-]
+db = load_db() 
 
 @app.get("/api/cars")
 def get_cars(size: str|None = None, doors: int|None = None) -> list:
