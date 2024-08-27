c69564250c2d4fb41c048a06ab6907d2e097d014
PhunktionalJohnkey <leyealexoni@gmail.com>
2024-08-27 11:04:46 +0000

    adds logic to filter cars on size and doors - expected to fail

--- carsharing.py
+++ carsharing.py
@@ -16,8 +16,11 @@ db = [
 ]
 
 @app.get("/api/cars")
-def get_cars(size=None):
+def get_cars(size=None, doors=None):
+    result = db
     if size:
-        return [car for car in db if car['size'] == size]
-    else:
-        return db
+        result =  [car for car in result if car['size'] == size]
+    if doors:
+        result =  [car for car in result if car['doors'] >= doors]
+    return result
+    
